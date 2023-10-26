d172f1f78971a724feaa8b9ea96856bdd1797a22
tereshkovyv <tyv2016@yandex.ru>
2023-10-26 12:35:58 +0500

    2-nd Hometask 2-nd attempt

---
+++ src/components/app/app.tsx
@@ -0,0 +1,7 @@
+import MainScreen from '../../pages/main-screen/main-screen';
+
+export default function App(){
+  return (
+    <MainScreen name={'The Grand Budapest Hotel poster'} genre={'Drama'} releaseDate={'2014'}/>
+  );
+}
