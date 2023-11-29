7814cdea57784e0eb2e79e185e3ddd24007a6884
tereshkovyv <tyv2016@yandex.ru>
2023-11-29 20:26:33 +0500

    Bug fixes

--- src/pages/sign-in-screen/sign-in-screen.tsx
+++ src/pages/sign-in-screen/sign-in-screen.tsx
@@ -1,38 +1,39 @@
 import Footer from '../../components/footer/footer.tsx';
 import Header from '../../components/header/header.tsx';
-import {useRef} from 'react';
+import {FormEvent, useState} from 'react';
 import {store} from '../../store';
 import {checkAuthAction, loginAction} from '../../store/api-actions.tsx';
 import {useAppSelector} from '../../hooks';
 import SignInError from './sign-in-error.tsx';
 
 export default function SignInScreen(){
-  const inputRef = useRef(null);
   const errors = useAppSelector((state) => state.error);
-  function onSubmit(evt){
+
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  function onSubmit(evt : FormEvent<HTMLFormElement>) {
     evt.preventDefault();
-    const userEmail = (new FormData(inputRef.current)).get('user-email');
-    const userPassword = (new FormData(inputRef.current)).get('user-password');
-    store.dispatch(loginAction({login : userEmail, password : userPassword}));
+    store.dispatch(loginAction({login : emailValue, password : passwordValue}));
     store.dispatch(checkAuthAction());
   }
+
   return (
     <div className="user-page">
       <Header/>
 
       <div className="sign-in user-page__content">
-        <form onSubmit={onSubmit} className="sign-in__form" ref={inputRef}>
+        <form onSubmit={onSubmit} className="sign-in__form" >
           {errors !== null && <SignInError/>}
           <div className="sign-in__fields">
             <div className="sign-in__field">
               <input className="sign-in__input" type="email" placeholder="Email address" name="user-email"
-                id="user-email"
+                id="user-email" value={emailValue} onChange={(e) => setEmailValue(e.target.value)}
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
               <input className="sign-in__input" type="password" placeholder="Password" name="user-password"
-                id="user-password"
+                id="user-password" value={passwordValue} onChange={(e) => setPasswordValue(e.target.value)}
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
