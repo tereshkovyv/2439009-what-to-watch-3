928e81a6e1b8216c8f28be57e5517648f804b00c
tereshkovyv <tyv2016@yandex.ru>
2023-12-29 15:51:40 +0500

    Bug fixes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -6,26 +6,21 @@ import MovieScreen from '../../pages/movie-screen/movie-screen';
 import PlayerScreen from '../../pages/player-screen/player-screen';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import {PrivateRoute, AuthorizationStatus} from '../private-route/private-route';
-import {filmData, filmsMock} from '../../mocks/films';
 import {playerMock} from '../../mocks/playerMock.ts';
 import AddReviewScreen from '../../pages/add-review-screen/add-review-screen.tsx';
 
 type appProps = {
-  name : string;
-  genre : string;
-  films : filmData[];
-  releaseDate : string;
   menuItems : string[];
 }
 
-export default function App({name, genre, films, releaseDate, menuItems} : appProps){
+export default function App({menuItems} : appProps){
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainScreen name={name} genre={genre} releaseDate={releaseDate} films={films} menuItems={menuItems}/>} />
+        <Route path='/' element={<MainScreen menuItems={menuItems}/>} />
         <Route path='/login' element={<SignInScreen/>} />
         <Route path='/myList' element={
-          <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}><MyListScreen films={filmsMock}/></PrivateRoute>
+          <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}><MyListScreen/></PrivateRoute>
         }
         />
         <Route path='/films/:id/*' element={<MovieScreen/> }/>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import {filmsMock} from './mocks/films';
 import {genresListMock} from './mocks/main-page-menu.ts';
 import {Provider} from 'react-redux';
 import {store} from './store';
@@ -13,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App name={'The Grand Budapest Hotel poster'} genre={'Drama'} releaseDate={'2014'} films={filmsMock} menuItems={genresListMock}/>
+      <App menuItems={genresListMock}/>
     </Provider>
   </React.StrictMode>
 
